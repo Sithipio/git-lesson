48fdc3afd2a6b62b763056de25e9112b2f9579cd
Sithipio <vberezos@gmail.com>
2022-04-06 23:50:52 +0300

    created alphabetical grid

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,21 @@
-function grid() {
-  //Here is your solution
-  return
+function grid(n) {
+    if (n > 0) {
+        let arr = [];
+
+        for (let i = 0; i < n; i++) {
+            let subArr = [];
+            for (let j = 0; j < n; j++) {
+                subArr.push(String.fromCharCode(97 + ((i + j) % 26)));
+            }
+            arr.push(subArr);
+        }
+        return arr;
+    }
+    return null;
 }
+
+
+console.log(grid(4));
+console.log(grid(10));
+console.log(grid(0));
+console.log(grid(-10));
