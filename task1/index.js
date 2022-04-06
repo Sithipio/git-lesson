50593e0b2ce0f44d582fd6cdc847d942e3881474
Sithipio <vberezos@gmail.com>
2022-04-06 23:13:10 +0300

    created dot calculator

--- task1/index.js
+++ task1/index.js
@@ -1,4 +1,40 @@
-function calculate() {
-  //Here is your solution
-  return
+function calculate(str) {
+  if (str.includes("+")) {
+    let additions = str.split(' + ');
+    return additions.join('');
+  }
+
+  let result = "";
+  let countDots = 0;
+
+  if (str.includes("-")) {
+    let arr = str.split(' - ');
+    countDots = arr[0].length - arr[1].length;
+  }
+
+  if (str.includes("*")) {
+    let arr = str.split(' * ');
+    countDots = arr[0].length * arr[1].length;
+  }
+
+  if (str.includes("//")) {
+    let arr = str.split(' // ');
+    countDots = ~~(arr[0].length/arr[1].length);
+  }
+
+  for (let n = 0; n < countDots; n++) {
+    result += ".";
+  }
+  return result;
 }
+
+
+console.log(calculate("..... + ..............."));
+console.log(calculate("..... - ..."));
+console.log(calculate("..... - ."));
+console.log(calculate("..... * ..."));
+console.log(calculate("..... * .."));
+console.log(calculate("..... // .."));
+console.log(calculate("..... // ."));
+console.log(calculate(". // .."));
+console.log(calculate(".. - .."));
