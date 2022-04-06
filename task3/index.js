15b18d9ebae105951a6af2e82ded912893e8a2f8
Sithipio <vberezos@gmail.com>
2022-04-07 00:12:23 +0300

    created valid parentheses

--- task3/index.js
+++ task3/index.js
@@ -1,3 +1,23 @@
-function isValidParentheses() {
-  //code
+function isValidParentheses(input) {
+    if (input.length >= 0 && input.length <= 100) {
+        let counter = 0;
+        for (let i = 0; i < input.length; i++) {
+            if (input[i] === "(") {
+                counter++;
+            } else if (input[i] === ")") {
+                counter--;
+            }
+            if (counter < 0) {
+                return false;
+            }
+        }
+        return counter === 0;
+    }
+    return false;
 }
+
+
+console.log(isValidParentheses("()"));
+console.log(isValidParentheses(")(()))"));
+console.log(isValidParentheses("("));
+console.log(isValidParentheses("(())((()())())"));
